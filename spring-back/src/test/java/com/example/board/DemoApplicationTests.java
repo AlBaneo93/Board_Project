f914527625d19a29d74144a144c445308b1011de
AlBan <albaneo0724@gmail.com>
2020-07-06 17:32:41 +0900

    aa

--- spring-back/src/test/java/com/example/board/DemoApplicationTests.java
+++ spring-back/src/test/java/com/example/board/DemoApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.board;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
 
+  }
 }
--- spring-image/src/main/java/com/example/board_image/BoardImageApplication.java
+++ spring-image/src/main/java/com/example/board_image/BoardImageApplication.java
@@ -1,13 +1,19 @@
 package com.example.board_image;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class BoardImageApplication {
 
+    @Autowired
+    Environment environment;
+
     public static void main(String[] args) {
         SpringApplication.run(BoardImageApplication.class, args);
+
     }
 
 }
