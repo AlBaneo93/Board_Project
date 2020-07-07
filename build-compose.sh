e97f1b3421d58da75129d3444491957005e65c32
AlBan <albaneo0724@gmail.com>
2020-07-08 01:17:44 +0900

    [/] update compose file

--- build-compose.sh
+++ build-compose.sh
@@ -2,13 +2,6 @@ cd ./spring-back
 ./gradlew clean build
 cd ../spring-image
 ./gradlew clean build
-
-# remote docker container
-docker rm -f $(docker ps -a -q)
-# remove before network
-docker network rm mynet
-
-# create project network
-docker network create mynet
+cd $
 # project deploy
 docker-compose up -d --force-recreate
