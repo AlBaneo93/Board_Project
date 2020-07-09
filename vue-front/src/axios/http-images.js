eaef2c911d19b3be24d438668d36b996f438d502
AlBan <albaneo0724@gmail.com>
2020-07-10 01:11:06 +0900

    [/] update config code

--- vue-front/src/axios/http-images.js
+++ vue-front/src/axios/http-images.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-	baseURL: 'http://localhost:10101/api',
+	baseURL: 'http://image:10101/api',
 	'Content-Type': 'multipart/form-data',
 });
