facff7cb53e62bf5c70f7fe7ceb9ae55d4d027cd
AlBan <albaneo0724@gmail.com>
2020-07-10 17:11:42 +0900

    [/] update compose file & created .env file

--- build-compose.sh
+++ build-compose.sh
@@ -2,6 +2,9 @@ cd ./spring-back
 ./gradlew clean build
 cd ../spring-image
 ./gradlew clean build
-cd $
+cd ../
+
+docker network create boardnet
+
 # project deploy
 docker-compose up -d --force-recreate
