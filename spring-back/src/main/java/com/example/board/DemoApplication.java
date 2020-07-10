a2e3e14afe017558658ab3634672215069dc793b
AlBan <albaneo0724@gmail.com>
2020-07-10 17:31:16 +0900

    [/] update restart strategy

--- spring-back/src/main/java/com/example/board/DemoApplication.java
+++ spring-back/src/main/java/com/example/board/DemoApplication.java
@@ -2,15 +2,15 @@ package com.example.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy // Enable AOP
-//@EnableCaching
+// @EnableCaching
 public class DemoApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(DemoApplication.class, args);
+
   }
 }
