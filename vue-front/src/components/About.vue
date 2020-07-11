dc3058b41ec13626e25cf5d3bf9753ba1801a8ac
AlBaneo93 <albaneo0724@gmail.com>
2020-07-12 00:44:05 +0900

    aa

--- vue-front/src/components/About.vue
+++ vue-front/src/components/About.vue
@@ -1,12 +1,37 @@
 <template>
 	<v-content>
-		<h1>About this service</h1>
-		<h2>안녕하세요</h2>
+		<!-- <transition> -->
+		<v-card>
+			<v-img
+				v-for="i in images"
+				:key="i"
+				height="600"
+				width="800"
+				:src="images[i]"
+			/>
+		</v-card>
+		<!-- </transition> -->
+		<!-- <transition> -->
+		<!-- <v-img height="600" width="800" :src="@/assets/images/kakaofriends_2.png" /> -->
+		<!-- </transition> -->
+		<!-- <transition> -->
+		<!-- <v-img height="600" width="800" :src="@/assets/images/kakaofriends_3.png" /> -->
+		<!-- </transition> -->
 	</v-content>
 </template>
 
 <script>
-export default {};
+export default {
+	data() {
+		return {
+			images: [
+				'@/assets/images/kakaofriends_1.png',
+				'@/assets/images/kakaofriends_2.png',
+				'@/assets/images/kakaofriends_3.png',
+			],
+		};
+	},
+};
 </script>
 
 <style></style>
